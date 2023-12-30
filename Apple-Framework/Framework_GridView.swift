8b49898ab52c28f40cd89865f5178dfe7023aa80
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-12-30 17:15:07 +0600

    create LazyVGrid() for showing items

---
+++ Apple-Framework/Framework_GridView.swift
@@ -0,0 +1,18 @@
+//
+//  Framework_GridView.swift
+//  Apple-Framework
+//
+//  Created by Hadiuzzaman on 30/12/23.
+//
+
+import SwiftUI
+
+struct Framework_GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Framework_GridView()
+}
