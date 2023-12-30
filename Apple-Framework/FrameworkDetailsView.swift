1595bc724842dad9282f3358e0177f099eaf9f16
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-12-30 19:53:00 +0600

    feat: design details screen

---
+++ Apple-Framework/FrameworkDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailsView.swift
+//  Apple-Framework
+//
+//  Created by Hadiuzzaman on 30/12/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailsView()
+}
