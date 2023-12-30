bd8383db956e264469c054cb3181171bbf371437
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-12-30 17:18:02 +0600

    create lazyvgrid

--- Apple-Framework/Framework_GridView.swift
+++ Apple-Framework/Framework_GridView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct Framework_GridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible()),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns){
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+            FrameworkTitle(imageName: "app-clip", name: "App Clips")
+        
+        }
+     
     }
 }
 
 #Preview {
     Framework_GridView()
+}
+
+
+struct FrameworkTitle: View {
+    
+    var imageName:String
+    var name:String
+    
+    var body: some View{
+        VStack{
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
 }
