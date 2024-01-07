1601cbc459285e37d06eef7be19e035fb7c2beaa
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2024-01-07 22:43:51 +0600

    feat: design form

--- Practice App/Appetizer/Appetizer/Screen/Account/AccountView.swift
+++ Practice App/Appetizer/Appetizer/Screen/Account/AccountView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var firstName: String = ""
+    @State var lastName: String = ""
+    @State var email: String = ""
+    @State var date: Date = Date.now
+    @State var extraNapkin: Bool = false
+    @State var frequentRefill: Bool = false
+    
     var body: some View {
         NavigationView{
-            Text("Account") .navigationTitle("Account")
+            Form{
+                Section(header: Text("User Information")){
+                    TextField("First name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday" ,selection: $date, displayedComponents: .date)
+                    Button{
+                        print("Button Tap")
+                    } label: {
+                       Text("Save Information")
+                    }
+                    
+                }
+                Section(header: Text("Additional info")){
+                    Toggle("Extra napkins", isOn: $extraNapkin )
+                    Toggle("Frequent refill", isOn: $frequentRefill )
+                }
+            }
+            .navigationTitle("🚀 Account")
         }
       
     }
