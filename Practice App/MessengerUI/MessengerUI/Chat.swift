dab3f7c9944a605d4d6046a5e0e7d74358fe2017
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2024-01-23 21:00:51 +0600

    feat: design chat screen

--- Practice App/MessengerUI/MessengerUI/Chat.swift
+++ Practice App/MessengerUI/MessengerUI/Chat.swift
@@ -8,23 +8,56 @@
 import SwiftUI
 
 struct Chat: View {
+    
+    private var nameList  = ["Hadi", "Tamanna", "Habib", "Rakib", "Rohan"]
+    
     @State var search : String = ""
     var body: some View {
         NavigationView{
-            VStack{
-                
-                HStack{
-                  Image("hadi")
-                        .resizable()
-                        .frame(width:100, height: 100)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            ScrollView{
+                VStack(spacing:8){
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            ForEach(nameList, id: \.self){
+                                name in
+                                VStack{
+                                    Image("hadi")
+                                        .resizable()
+                                        .frame(width:80, height: 80)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                        .shadow(radius:2)
+                                    Text(name)
+                                }
+                                
+                            }
+                            
+                            
+                        }
+                    }
                     
-                   Circle()
-                   Circle()
-                   Circle()
+                  
+                    ForEach(nameList, id: \.self){ name in
+                        HStack(spacing:12){
+                            Image("hadi")
+                                .resizable()
+                                .frame(width: 70, height: 70)
+                                .clipShape(Circle())
+                           
+                            VStack(alignment: HorizontalAlignment.leading){
+                                Text(name)
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                Text("Joined: 12/04/2023")
+                            }
+                            
+                                
+                            Spacer()
+                            
+                        }.padding(.horizontal)
+                    }
+                    
                 }
-                Spacer()
-            }.padding(.horizontal)
+            }
            
                 .navigationTitle("🥱 Chat")
         }.searchable(text: $search)
