737d9d0e574cae80b8f4fa701924d42d805175da
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2024-01-25 22:16:47 +0600

    refactor: design contact screen

--- Practice App/MessengerUI/MessengerUI/Screen/People/People.swift
+++ Practice App/MessengerUI/MessengerUI/Screen/People/People.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct People: View {
+    
+    @StateObject var viewModel = PersonViewModel()
+    
     var body: some View {
-        Text("People")
+        NavigationView{
+            ScrollView{
+                VStack{
+                    ForEach(viewModel.personList, id:\.self){
+                        person in
+                        HStack(spacing:12){
+                            Image("hadi")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .clipShape(Circle())
+                            VStack(alignment: HorizontalAlignment.leading){
+                                Text(person)
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                    
+                                Text("Joined: 01/12/2023")
+                            }
+                           
+                       Spacer()
+                        }
+                    }.padding(.horizontal)
+                }
+               
+            }
+            .navigationTitle("Contacts")
+        }
+        
     }
 }
 
