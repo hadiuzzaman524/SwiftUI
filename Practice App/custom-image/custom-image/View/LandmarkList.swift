6544e70653f246ed93b4299b8ad29fb3e195db0f
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2024-03-04 08:23:04 +0600

    design list for show landmark

---
+++ Practice App/custom-image/custom-image/View/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  custom-image
+//
+//  Created by Hadiuzzaman on 4/3/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
